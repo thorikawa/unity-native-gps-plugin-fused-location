a600a887338208c55dfb687bdecc39e84ea1ecfd
Takahiro Poly Horikawa <poly@curiosity-inc.jp>
2023-01-11 15:19:26 +0900

    Update to Android's Fused Location Provider.

--- Android/LocaleTools/src/main/java/com/marcinkulwicki/localetools/Location.java
+++ Android/LocaleTools/src/main/java/com/marcinkulwicki/localetools/Location.java
@@ -1,26 +1,35 @@
 package com.marcinkulwicki.localetools;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
-import android.location.LocationManager;
-import android.location.LocationListener;
+
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.Priority;
 
 public class Location implements LocationListener
 {
     public static android.location.Location LastLocation;
+    private FusedLocationProviderClient locationManager;
 
-    private LocationManager locationManager;
-
+    @SuppressLint("MissingPermission")
     public void init(Context context)
     {
-        locationManager = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
-        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 0, this);
-        locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 1000, 0, this);
-        locationManager.requestLocationUpdates(LocationManager.PASSIVE_PROVIDER, 1000, 0, this);
+        locationManager = LocationServices.getFusedLocationProviderClient(context);
+        LocationRequest.Builder locationRequest = new LocationRequest.Builder(500);
+        locationRequest.setWaitForAccurateLocation(true);
+        locationRequest.setPriority(Priority.PRIORITY_HIGH_ACCURACY);
+        locationManager.requestLocationUpdates(locationRequest.build(), this, context.getMainLooper());
     }
 
     @Override
-    public void onLocationChanged(android.location.Location location)
-    {
+    public void onLocationChanged(@NonNull android.location.Location location) {
         LastLocation = location;
     }
 }
